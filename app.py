7169dcce4fa145d948ec05a74064d802c9d02239
captaincrazybro <myeyesareonmizar@gmail.com>
2023-03-30 16:37:18 -0400

    Fixed another syntax error

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def post_handle():
     y = json["controller_y"]
     r1 = json["controller_r1"]
     l1 = json["controller_l1"]
-    dup = json["constroller_dub"]
+    dup = json["constroller_dup"]
     ddown = json["controller_ddown"]
 
     beltSpeed = 0
