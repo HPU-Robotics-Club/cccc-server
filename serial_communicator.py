35a7fea6429c1f8f927f545540de5836d62a50b2
captaincrazybro <myeyesareonmizar@gmail.com>
2023-03-30 16:38:38 -0400

    Added new serial communicator code

--- serial_communicator.py
+++ serial_communicator.py
@@ -2,36 +2,35 @@ import string
 import serial
 from enum import Enum
 
-PORT = '/dev/ttyACM0'
+PORT = '/dev/ttyACM0' 
 DIVIDER = "|"
 BAUDRATE = 115200
 
 class SerialCommunicator:
     def __init__(self):
-        print("Starting serial communicator...")
-        # self.arduino = serial.Serial(PORT, BAUDRATE)
+        #self.arduino = serial.Serial(PORT, BAUDRATE)
         print("Starting serial communicator...")
 
         self.prev_left_speed = 0
         self.prev_right_speed = 0
+        self.prev_belt_speed = 0
+        self.prev_act_speed = 0
 
     def write(self, msg: str):
         code = f'{msg}{DIVIDER}'
-        # self.arduino.write(code.encode())
+        #self.arduino.write(code.encode())
         print(f'{code}')
-        return code
     
-    def write_motor_command(self, left_motor_code: str, right_motor_code: str, left_motor_value: int, right_motor_value: int):
+    def write_motor_command(self, left_motor_code: str, right_motor_code: str, belt_motor_code: str, act_motor_code: str, left_motor_value: int, right_motor_value: int, belt_motor_value: int, act_motor_value: int):
         # This if statement makes it so that it wont send duplicate motor commands right after each other
-        if (left_motor_value != self.prev_left_speed or right_motor_value != self.prev_right_speed):
-            code = self.write(f'{left_motor_code}{self.format_motor_value(left_motor_value)}{right_motor_code}{self.format_motor_value(right_motor_value)}')
+        if (left_motor_value != self.prev_left_speed or right_motor_value != self.prev_right_speed or belt_motor_value != self.prev_belt_speed or act_motor_value != self.prev_act_speed):
+            self.write(f'{left_motor_code}{self.format_motor_value(left_motor_value)}{right_motor_code}{self.format_motor_value(right_motor_value)}{belt_motor_code}{self.format_motor_value(belt_motor_value)}{act_motor_code}{self.format_motor_value(act_motor_value)}')
             
             # Assigns current motor values to the previous motor value variables
             self.prev_left_speed = left_motor_value
             self.prev_right_speed = right_motor_value
-            return code
-        
-        return "Duplicate Sent!"
+            self.prev_belt_speed = belt_motor_value
+            self.prev_act_speed = act_motor_value
 
     def format_motor_value(self, motor_value: int):
         N = 0
@@ -49,3 +48,7 @@ class SerialCommunicator:
 class MotorCode():
     WHEELS_FORWARD = "wf"
     WHEELS_BACKWARD = "wb"
+    BELT_FORWARD = "bf"
+    BELT_BACKWARD = "bb"
+    ACT_FORWARD = "af"
+    ACT_BACKWARD = "ab"
